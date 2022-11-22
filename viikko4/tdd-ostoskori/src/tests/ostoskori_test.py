08417a1728ac9b4b95b54067ce92bc4629a0a277
Samu Syrjänen <samu.syrjanen@gmail.com>
2022-11-22 13:44:56 +0200

    vko 4 tehtävä 5.3: Yhden tuotteen lisäämisen jälkeen ostoskorin hinta on sama kuin tuotteen hinta

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -14,3 +14,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_oikea_hinta(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.hinta(), 3)
